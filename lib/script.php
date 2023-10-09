504a0b8d48bccf8b275151027941b8de3aad2476
John Doe <johndoe@email.com>
2023-10-09 14:42:03 +0530

    ok

--- lib/script.php
+++
@@ -1,4 +0,0 @@
- <!-- JavaScript -->
- <script src="./src/assets/js/bundle.js?ver=3.2.0"></script>
- <script src="./src/assets/js/scripts.js?ver=3.2.0"></script>
- <script src="./src/assets/js/charts/chart-ecommerce.js?ver=3.2.0"></script>
